d933f28d46a7ed350582c37d94ab3044f4269ca7
studTon <bra.17.ess@gmail.com>
2021-08-20 20:16:34 -0300

    Exercise about variables Number 1

---
+++ script.js
@@ -0,0 +1,8 @@
+let characterAge = 48;
+console.log(characterAge);
+let characterHeight = 1.75;
+console.log(characterHeight);
+let characterName = 'David';
+console.log(characterName);
+let ill = true;
+console.log('Ill?: ' + ill)
