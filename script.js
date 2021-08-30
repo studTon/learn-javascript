0d19d61861fe519a8cbb60cb2fb4824df3c00287
studTon <bra.17.ess@gmail.com>
2021-08-30 20:11:19 -0300

    Exercícios de constantes até arrays constantes, tipos primitivos, tipagem dinâmica, objetos e arrays

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//variáveis
 let characterAge = 48;
 console.log(characterAge);
 let characterHeight = 1.75;
@@ -5,4 +6,36 @@ console.log(characterHeight);
 let characterName = 'David';
 console.log(characterName);
 let ill = true;
-console.log('Ill?: ' + ill)
+console.log('Ill?: ' + ill);
+//constantes
+const Pi = 3.14159;
+console.log(Pi);
+//tipos primitivos
+let palavra = 'Porta';
+let numero = 32.5;
+let aprovado = true;
+let erros = undefined;
+let acertos = null;
+console.log(palavra);
+console.log(numero);
+console.log(aprovado);
+console.log(erros);
+console.log(acertos);
+//tipagem dinâmica
+console.log(typeof palavra);
+console.log(typeof numero);
+console.log(typeof aprovado);
+console.log(typeof erros);
+console.log(typeof acertos);
+//objetos
+let cor = {
+	azul: 180,
+	verde: 200,
+	vermelho: 50,
+	nome: 'azul celeste',
+	tom: 'azulado'
+};
+console.log(cor);
+//arrays
+let triangulo = [28, 62, 90];
+console.log(triangulo);
